c67d2d7d6ba7a29a1be999340e106d72c67943d5
Josumaru <jokosupriyanto1712@gmail.com>
2023-10-20 01:22:40 +0700

    create navigation bar

--- app/src/main/java/com/onogawean/sun/activities/MainActivity.java
+++ app/src/main/java/com/onogawean/sun/activities/MainActivity.java
@@ -31,8 +31,5 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(new Intent(MainActivity.this, LoginRegisterActivity.class));
             }
         });
-
-
-
     }
 }
