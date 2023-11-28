ecdad5f87c84f285f3305dd31a356f1bb925fb01
Fad <fadhilerdyaqasmal@gmail.com>
2023-11-28 23:00:47 +0700

    Doing Something to Login page :sob:

--- app/src/main/java/com/onogawean/sun/fragment/LoginFragment.java
+++ app/src/main/java/com/onogawean/sun/fragment/LoginFragment.java
@@ -5,12 +5,17 @@ import android.os.Bundle;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.text.TextUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.material.textfield.TextInputEditText;
 import com.onogawean.sun.R;
 
 import com.onogawean.sun.activities.LoginRegisterActivity;
@@ -31,6 +36,8 @@ public class LoginFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
+
+
     public LoginFragment() {
         // Required empty public constructor
     }
@@ -44,7 +51,7 @@ public class LoginFragment extends Fragment {
      * @return A new instance of fragment LoginFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static LoginFragment newInstance(String param1, String param2) {
+    protected static LoginFragment newInstance(String param1, String param2) {
         LoginFragment fragment = new LoginFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
@@ -59,14 +66,38 @@ public class LoginFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_login, container, false);
 
+        View view = inflater.inflate(R.layout.fragment_login, container, false);
         ImageView loginViewButton = view.findViewById(R.id.ic_arrow_login);
+        EditText emailText, passText;
+        Button submitButton;
+
+
+        emailText = view.findViewById(R.id.login_email);
+        passText = view.findViewById(R.id.login_password);
+        submitButton = view.findViewById(R.id.login_button);
+
+        submitButton.setOnClickListener(v ->{
+            String email, pass;
+            email = String.valueOf(emailText.getText());
+            pass = String.valueOf(passText.getText());
+            TextView emptyText = view.findViewById(R.id.emptyText);
+
+            if(TextUtils.isEmpty(email)){
+                emptyText.setVisibility(View.VISIBLE);
+            }
+
+            if(TextUtils.isEmpty(pass)){
+                emptyText.setVisibility(View.VISIBLE);
+            }
+
+        });
 
         loginViewButton.setOnClickListener(v -> {
             Fragment registerFragment = new RegisterFragment();
@@ -75,7 +106,6 @@ public class LoginFragment extends Fragment {
             //fragmentTransaction.setCustomAnimations(android.R.anim.slide_in_left, android.R.anim.slide_out_right);
             fragmentTransaction.replace(R.id.login_register_fragment, registerFragment).commit();
         });
-
         return view;
     }
 }
