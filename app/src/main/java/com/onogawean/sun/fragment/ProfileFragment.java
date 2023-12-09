cca708d5d8093558d459eda6a8afc7d78a796ddb
Fad <fadhilerdyaqasmal@gmail.com>
2023-12-09 22:17:01 +0700

    more login

--- app/src/main/java/com/onogawean/sun/fragment/ProfileFragment.java
+++ app/src/main/java/com/onogawean/sun/fragment/ProfileFragment.java
@@ -21,11 +21,17 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+
+
+import com.google.firebase.database.ValueEventListener;
 import com.onogawean.sun.R;
 import com.onogawean.sun.activities.MainActivity;
 
@@ -75,9 +81,36 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        TextView usernameText = view.findViewById(R.id.username_text);
+        TextView emailText = view.findViewById(R.id.email_text);
+        DatabaseReference databaseReference ;
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
 
-        TextView username = view.findViewById(R.id.username_text);
-        TextView pass = view.findViewById(R.id.email_text);
+        // Get the user ID
+        String userId = currentUser.getUid();
+        String email = currentUser.getEmail();
+        emailText.setText(email);
+
+        // Get a reference to the user's node in the database
+        databaseReference = FirebaseDatabase.getInstance().getReference("mahasiswa");
+
+        if (currentUser != null) {
+            databaseReference.orderByChild("email").equalTo(email).addListenerForSingleValueEvent(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    for (DataSnapshot dataSnapshot : snapshot.getChildren()) {
+                        if (dataSnapshot.exists()) {
+                            String nama = dataSnapshot.child("name").getValue(String.class);
+                            usernameText.setText(nama);
+
+                        }
+                    }
+                }
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                }
+            });
+        }
 
         Button signOutButton = view.findViewById(R.id.signout_button);
         signOutButton.setOnClickListener(v -> {
