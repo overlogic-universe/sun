3f429e6ddfe608836194c73a0ab0e12184d9d176
RizkyMaulanaS <rizkyzky345@gmail.com>
2023-12-13 21:34:22 +0700

    Note Updated

---
+++ app/src/main/java/com/onogawean/sun/fragment/Utility.java
@@ -0,0 +1,21 @@
+package com.onogawean.sun.fragment;
+import android.content.Context;
+import android.widget.Toast;
+
+import com.google.firebase.Firebase;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+
+public class Utility {
+    static void showToast(Context context,String message){
+        Toast.makeText(context,message,Toast.LENGTH_SHORT).show();
+    }
+    static CollectionReference getCollectionRefNote(){
+        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+        return FirebaseFirestore.getInstance().collection("notes").document(currentUser.getUid()).collection("my_notes");
+
+    }
+}
